19f1dbc2332d052f0e9360be66a683d4caf2abb8
Samuel Stenton <sthstenton@gmail.com>
2018-11-04 12:45:57 +0000

    make a pretty

--- db/db-build.sql
+++ db/db-build.sql
@@ -5,10 +5,8 @@ CREATE TABLE public.unlocks
 	description varchar,
     peopleRequired real,
     currencYRequired real,
-    peopleMultiplyer real,
-    currencyMultiplyer real,
-    peoplePerSecond real,
-    currencyPerSecond real,   
+    peopleMultiplier real,
+    currencyMultiplier real,
 	peopleCost real,
 	currencyCost real,
 	prerequisite integer,
@@ -16,25 +14,41 @@ CREATE TABLE public.unlocks
 	FOREIGN KEY (prerequisite) REFERENCES unlocks(id)
 );
 
-CREATE TABLE IF NOT EXISTS "unlocks_1" (
-    "id" NUMERIC(2, 1),
-    "title" TEXT,
-    "description" TEXT,
-    "peopleRequired" NUMERIC(4, 1),
-    "currencyRequired" NUMERIC(2, 1),
-    "peopleMultiplyer" NUMERIC(3, 2),
-    "currencyMultiplyer" NUMERIC(2, 1),
-    "peoplePerSecond" NUMERIC(2, 1),
-    "currencyPerSecond" NUMERIC(2, 1),
-    "peopleCost" NUMERIC(2, 1),
-    "currencyCost" NUMERIC(2, 1),
-    "prerequisite" INT
+CREATE TABLE user(
+    id varchar(35) PRIMARY KEY,
+    password varchar(35),
+    empireName varchar(50),
+    currencyName varchar(35)
 );
+
+CREATE TABLE planet(
+    id INT PRIMARY KEY, 
+    name varchar(40),
+    user varchar(35),
+    curreny real,
+    people real,
+    currencyPerSecond real,
+    peoplePerSecond real,
+
+    FOREIGN KEY (name) REFERENCES user(id)
+);
+
+
+CREATE TABLE leaderboard(
+    userId varchar(35) PRIMARY KEY,
+    currency real,
+    people real,
+
+    FOREIGN KEY (userId) REFERENCES user(id)
+);
+
+
 INSERT INTO "unlocks" VALUES
-    (1.0,'Sex','Fun times ahead',2.0,0.0,1.25,0.0,1.0,0.0,0.0,0.0,NULL),
-    (2.0,'Language','wot',100.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,1),
-    (3.0,'Stone Tools','Stone cut things',150.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,NULL),
-    (4.0,'Reading/Writing','Learnt to speeel',250.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,2),
-    (5.0,'Foraging','Dem leafs tastee.',300.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,NULL),
-    (6.0,'Hunting','Meat good, tummy hurt',350.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,3),
-    (7.0,'Cooking','Meat good, tummy no hurt',400.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,7);
+    (1.0,'Sex','Fornication for the nation',2.0,0.0,0.25,0.0,0.0,0.0,NULL),
+    (2.0,'Language','wot',100.0,0.0,0.1,0.0,0.0,0.0,1),
+    (3.0,'Stone Tools','Stone cut things',150.0,0.0,0.1,0.0,0.0,0.0,NULL),
+    (4.0,'Reading/Writing','Learnt to speeel',250.0,0.0,0.1,0.0,0.0,0.0,2),
+    (5.0,'Foraging','Dem leafs tastee.',300.0,0.0,0.1,0.0,0.0,0.0,NULL),
+    (6.0,'Hunting','Meat good, tummy hurt',350.0,0.0,0.1,0.0,100.0,0.0,3),
+    (7.0,'Cooking','Meat good, tummy no hurt',400.0,0.0,0.1,0.0,20.0,0.0,7),
+    (8.0,'Farms','Cow go moo; wheat go up',750.0,0.0,0.4,0.0,20.0,0.0,5);
